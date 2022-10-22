15007a7132513e9b07d57ec78353376a3cf8c9bf
Thiago Bezerra de Carvalho <thiagaonaruto@gmail.com>
2022-10-21 22:35:58 -0300

    quase bati em mim por erra em algo pequeno

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("o objeto está se movendo(S/N)?");
+
+bool estaSeMovendo = Console.ReadLine()!.Trim().Substring(0, 1).ToUpper() == "S";
+
+Console.Write("Deveria? ");
+
+bool deveria = Console.ReadLine()!.Trim().Substring(0, 1).ToUpper() == "S";
+
+string mensagem;
+
+if (!estaSeMovendo)
+{
+    if(!deveria)
+    {
+        mensagem = "Ótimo!";
+    }
+    else
+    {
+        mensagem = "Use WD-40.";
+         
+    }
+}
+else
+{
+    if(deveria)
+    {
+         mensagem = "Ótimo!";
+    }
+    else
+    {
+        mensagem = "Use Silver Tape.";
+    }
+}    
+
+Console.WriteLine(mensagem);
